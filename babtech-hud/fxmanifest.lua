9f7285a9a49cc25d2f79ec08fd57bc54afa37caa
Babun <194414856+elbabun@users.noreply.github.com>
2025-03-19 01:47:09 +0300

    updated server.lua
    updated server.lua

--- babtech-hud/fxmanifest.lua
+++ babtech-hud/fxmanifest.lua
@@ -1,5 +1,7 @@
 fx_version 'cerulean'
 game 'gta5'
+lua54 "yes"
+
 
 author 'Babun & Tech'
 name 'Hud'
@@ -10,6 +12,7 @@ client_scripts {
     'config.lua',
     'client.lua'
 }
+
 
 server_script 'server.lua'
 
--- babtech-hud/server.lua
+++ babtech-hud/server.lua
@@ -1,63 +1,39 @@
+QBCore = exports['qb-core']:GetCoreObject()
 
 RegisterServerEvent('hudevents:leftVehicle')
-AddEventHandler('hudevents:leftVehicle', function()	
+AddEventHandler('hudevents:leftVehicle', function()
     TriggerClientEvent('hudevents:leftVehicle', source)
 end)
-QBCore = exports['qb-core']:GetCoreObject()
+
 RegisterServerEvent('hudevents:enteredVehicle')
-AddEventHandler('hudevents:enteredVehicle', function(currentVehicle, currentSeat, vehicle_name, net_id)	
+AddEventHandler('hudevents:enteredVehicle', function(currentVehicle, currentSeat, vehicle_name, net_id)
     TriggerClientEvent('hudevents:enteredVehicle', source, currentVehicle, currentSeat, vehicle_name, net_id)
 end)
 
 function GetPlayer(src)
-    local QBCore = exports['qb-core']:GetCoreObject() 
-    return QBCore.Functions.GetPlayer(src) 
+    return QBCore.Functions.GetPlayer(src)
 end
 
+function GetPlayerData(user)
+    if not user then return nil end
 
-function GetName(user)
-    local QBCore = exports['qb-core']:GetCoreObject()
-    return user.PlayerData.charinfo.firstname..' '..user.PlayerData.charinfo.lastname
+    return {
+        name = user.PlayerData.charinfo.firstname .. ' ' .. user.PlayerData.charinfo.lastname .. ' ('..user.PlayerData.source..')',
+        cash = user.Functions.GetMoney('cash'),
+        bank = user.Functions.GetMoney('bank'),
+        job = user.PlayerData.job.label .. ' - ' .. user.PlayerData.job.grade.name
+    }
 end
-
-function GetJob(user)
-    local QBCore = exports['qb-core']:GetCoreObject()
-    return user.PlayerData.job.label..' - '..user.PlayerData.job.grade.name
-end
-
-function GetCash(user)
-    local QBCore = exports['qb-core']:GetCoreObject()
-    return user.Functions.GetMoney('cash')
-end
-
-function GetBank(user)
-    local QBCore = exports['qb-core']:GetCoreObject()
-    return user.Functions.GetMoney('bank')
-end
-
-function GetAllPlayers()
-    local QBCore = exports['qb-core']:GetCoreObject()
-    return QBCore.Functions.GetPlayers()
-end
-
-
 
 QBCore.Functions.CreateCallback('babun-hud-get-datas', function(source, cb)
-    local src = source
-    local user = GetPlayer(src)
-    if not user then 
+    local user = GetPlayer(source)
+    if not user then
         cb(nil)
         return
     end
 
-    local data = {
-        name = GetName(user) .. ' ('..src..')',
-        cash = GetCash(user),
-        bank = GetBank(user),
-        players = #GetAllPlayers(),
-        job = GetJob(user)
-    }
+    local data = GetPlayerData(user)
+    data.players = #QBCore.Functions.GetPlayers()
+
     cb(data)
 end)
-
-
